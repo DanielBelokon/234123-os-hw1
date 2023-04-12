ac26a13c680c21cf867829a4298105cc046a9abd
boazfrider <boazfrider@gmail.com>
2023-04-12 17:12:57 +0300

    deleted constructor

--- Commands.cpp
+++ Commands.cpp
@@ -1,10 +1,7 @@
 #include "Commands.h"
 
 // TODO: Add your implementation for classes in Commands.h
-Command::Command(const char *cmd_line)
-{
-    _firstWordOfCommand = cmd_line;
-}
+
 
 Command::~Command()
 {
