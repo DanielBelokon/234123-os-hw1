6ea2c60f70a8755bdbab8853f654da8f851c1907
Daniel Belokon <daniel.belokon@gmail.com>
2023-04-18 10:14:33 +0300

    Add test cases based on pdf

--- tests/BuiltInCommandsTests.cpp
+++ tests/BuiltInCommandsTests.cpp
@@ -2,12 +2,48 @@
 #include "BuiltInCommands.h"
 #include <string>
 
-TEST(BuiltInCommandsTests, TestBuiltInCommands)
+TEST(ChpromptTests, NewPrompt)
 {
-    EXPECT_EQ(0, 0);
+    ChangePromptCommand command = ChangePromptCommand("chprompt newPrompt");
+    command.execute();
+    EXPECT_EQ(SmallShell::getInstance().getPrompt(), "newPrompt> ");
 }
 
-TEST(GetCurrentDirectoryTests, TestGetCurrentDirectory)
+TEST(ChpromptTests, NoArgs)
+{
+    ChangePromptCommand command = ChangePromptCommand("chprompt");
+    command.execute();
+    EXPECT_EQ(SmallShell::getInstance().getPrompt(), "smash> ");
+}
+
+TEST(ChpromptTests, TooManyArgs)
+{
+    ChangePromptCommand command = ChangePromptCommand("chprompt newPrompt test");
+    command.execute();
+    EXPECT_EQ(SmallShell::getInstance().getPrompt(), "newPrompt> ");
+}
+
+// TODO: empty string for chprompt?
+
+TEST(ShowPidTests, BasicShowPid)
+{
+    ShowPidCommand command = ShowPidCommand("showpid");
+    std::ostringstream out;
+    command.setOutputStream(&out);
+    command.execute();
+    EXPECT_EQ(out.str(), "smash pid is " + std::to_string(getpid()) + "\n");
+}
+
+TEST(ShowPidTests, IgnoreArgs)
+{
+    ShowPidCommand command = ShowPidCommand("showpid arg1 arg2 arg3");
+    std::ostringstream out;
+    command.setOutputStream(&out);
+    command.execute();
+    EXPECT_EQ(out.str(), "smash pid is " + std::to_string(getpid()) + "\n");
+}
+
+TEST(PwdTests, BasicPwd)
 {
     GetCurrDirCommand command = GetCurrDirCommand("pwd");
     std::ostringstream out;
@@ -15,13 +51,40 @@ TEST(GetCurrentDirectoryTests, TestGetCurrentDirectory)
     // Get the current working directory, and store it in a string.
     char buffer[256];
     getcwd(buffer, sizeof(buffer));
-    std::string actual(buffer);
+    std::string expected(buffer);
 
     command.execute();
-    EXPECT_EQ(out.str(), actual + "\n");
+    EXPECT_EQ(out.str(), expected + "\n");
 }
 
-TEST(ChangeCurrentDirectoryTests, BasicChangeDirectory)
+TEST(PwdTests, IgnoreArgs)
+{
+    GetCurrDirCommand command = GetCurrDirCommand("pwd arg1 arg2 arg3");
+    std::ostringstream out;
+    command.setOutputStream(&out);
+    // Get the current working directory, and store it in a string.
+    char buffer[256];
+    getcwd(buffer, sizeof(buffer));
+    std::string expected(buffer);
+
+    command.execute();
+    EXPECT_EQ(out.str(), expected + "\n");
+}
+
+TEST(PwdTests, ChangeDirectory)
+{
+    ChangeDirCommand command = ChangeDirCommand("cd /home");
+    command.execute();
+
+    GetCurrDirCommand command2 = GetCurrDirCommand("pwd");
+    std::ostringstream out;
+    command2.setOutputStream(&out);
+    command2.execute();
+
+    EXPECT_EQ(out.str(), "/home\n");
+}
+
+TEST(CdTests, BasicChangeDirectory)
 {
     ChangeDirCommand command = ChangeDirCommand("cd /home");
     command.execute();
@@ -33,7 +96,7 @@ TEST(ChangeCurrentDirectoryTests, BasicChangeDirectory)
     EXPECT_EQ(actual, "/home");
 }
 
-TEST(ChangeCurrentDirectoryTests, ChangePreviousDirectory)
+TEST(CdTests, ChangePreviousDirectory)
 {
     std::string path = "cd " + SmallShell::getInstance().getWorkingDir();
     ChangeDirCommand command = ChangeDirCommand(path.c_str());
@@ -52,7 +115,7 @@ TEST(ChangeCurrentDirectoryTests, ChangePreviousDirectory)
     EXPECT_EQ(actual, SmallShell::getInstance().getWorkingDir());
 }
 
-TEST(ChangeCurrentDirectoryTests, PrevFailed)
+TEST(CdTests, PrevFailed)
 {
     SmallShell::getInstance().setPrevPath("");
 
@@ -65,7 +128,7 @@ TEST(ChangeCurrentDirectoryTests, PrevFailed)
     EXPECT_EQ(out.str(), "smash error: cd: OLDPWD not set\n");
 }
 
-TEST(ChangeCurrentDirectoryTests, TooManyArgs)
+TEST(CdTests, TooManyArgs)
 {
     ChangeDirCommand command = ChangeDirCommand("cd / test");
 
@@ -76,8 +139,30 @@ TEST(ChangeCurrentDirectoryTests, TooManyArgs)
     EXPECT_EQ(out.str(), "smash error: cd: too many arguments\n");
 }
 
+TEST(CdTests, NonExistentDirectory)
+{
+    ChangeDirCommand command = ChangeDirCommand("cd /test");
+
+    std::ostringstream out;
+    command.setOutputStream(&out);
+    command.execute();
+
+    EXPECT_EQ(out.str(), "smash error: cd: /test: No such file or directory\n");
+}
+
+TEST(CdTests, EmptyString)
+{
+    ChangeDirCommand command = ChangeDirCommand("cd");
+
+    std::ostringstream out;
+    command.setOutputStream(&out);
+    command.execute();
+    // TODO: is this the correct output?
+    EXPECT_EQ(out.str(), "smash error: cd: too few arguments\n");
+}
+
 // Test jobs command
-TEST(JobsListTests, TestJobsList)
+TEST(JobsTests, BasicList)
 {
     ExternalCommand *command1 = new ExternalCommand("sleep 1");
     ExternalCommand *command2 = new ExternalCommand("sleep 2");
@@ -93,37 +178,96 @@ TEST(JobsListTests, TestJobsList)
 
     // compare by line
 
-    std::string expected = out.str();
-    std::string actual;
-    actual += "[1]sleep : " + std::to_string(command1->getPid()) + " 0 secs\n";
-    actual += "[2]sleep : " + std::to_string(command2->getPid()) + " 0 secs\n";
-    actual += "[3]sleep : " + std::to_string(command3->getPid()) + " 0 secs\n";
+    std::string actual = out.str();
+    std::string expected;
+    expected += "[1]sleep : " + std::to_string(command1->getPid()) + " 0 secs\n";
+    expected += "[2]sleep : " + std::to_string(command2->getPid()) + " 0 secs\n";
+    expected += "[3]sleep : " + std::to_string(command3->getPid()) + " 0 secs\n";
 
     EXPECT_EQ(expected, actual);
 }
 
+TEST(JobsTests, IgnoreArgs)
+{
+    JobsCommand command = JobsCommand("jobs arg1 arg2 arg3");
+    std::ostringstream out;
+    command.setOutputStream(&out);
+    command.execute();
+    EXPECT_EQ(out.str(), "");
+}
+
+// TODO: adding stopped job
+// TODO: test sorted by id
+// TODO: test removing finished jobs
+
 // Test fg command
 
-TEST(ForegroundCommandTests, TestForegroundCommand)
+TEST(FgTests, BasicForeground)
 {
-    ExternalCommand *command1 = new ExternalCommand("sleep 1");
-    ExternalCommand *command2 = new ExternalCommand("sleep 2");
-    ExternalCommand *command3 = new ExternalCommand("sleep 3");
-    SmallShell::getInstance().getJobsList().addJob(command1, false);
-    SmallShell::getInstance().getJobsList().addJob(command2, false);
-    SmallShell::getInstance().getJobsList().addJob(command3, false);
+    ExternalCommand *command1 = new ExternalCommand("sleep 2 &");
+    command1->execute();
 
-    ForegroundCommand command = ForegroundCommand("fg 2");
+    ForegroundCommand command = ForegroundCommand("fg");
     std::ostringstream out;
     command.setOutputStream(&out);
     command.execute();
 
     // compare by line
 
-    std::string expected = out.str();
-    std::string actual;
+    std::string actual = out.str();
+    std::string expected;
 
-    actual += "sleep : " + std::to_string(command2->getPid()) + "\n";
+    expected += "sleep : " + std::to_string(command1->getPid()) + "\n";
 
     EXPECT_EQ(expected, actual);
+}
+
+TEST(FgTests, ForegroundId)
+{
+    ExternalCommand *command1 = new ExternalCommand("sleep 2 &");
+    command1->execute();
+
+    ForegroundCommand command = ForegroundCommand("fg 1");
+    std::ostringstream out;
+    command.setOutputStream(&out);
+    command.execute();
+
+    // compare by line
+
+    std::string actual = out.str();
+    std::string expected;
+
+    expected += "sleep : " + std::to_string(command1->getPid()) + "\n";
+
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(FgTests, StoppedJob)
+{
+    ExternalCommand *command1 = new ExternalCommand("sleep 2 &");
+    SmallShell::getInstance().getJobsList().addJob(command1, true);
+
+    ForegroundCommand command = ForegroundCommand("fg 1");
+    std::ostringstream out;
+    command.setOutputStream(&out);
+    command.execute();
+
+    // compare by line
+
+    std::string actual = out.str();
+    std::string expected;
+
+    expected += "sleep : " + std::to_string(command1->getPid()) + "\n";
+
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(FgTests, InvalidId)
+{
+    ForegroundCommand command = ForegroundCommand("fg 1");
+    std::ostringstream out;
+    command.setOutputStream(&out);
+    command.execute();
+
+    EXPECT_EQ(out.str(), "smash error: fg: job-id 1 does not exist\n");
 }
